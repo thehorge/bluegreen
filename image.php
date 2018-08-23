1e51bb76966efa223a13785a757e7273aa82674b
thehorge <42652141+thehorge@users.noreply.github.com>
2018-08-23 13:41:05 -0400

    Actually making it green.

--- image.php
+++ image.php
@@ -3,7 +3,8 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
-$deployment = getenv("green");
+//$deployment = getenv("green");
+$deployment = "green";
 
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
